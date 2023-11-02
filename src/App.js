2f816782822064b45023f8ad51646d02c2cbf682
Monila8 <monicalopezarroyave@gmail.com>
2023-11-02 12:50:32 -0500

    memorym game complete

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from "react"
+import "./App.css"
+import SingleCard from "./components/SingleCard.js"
+
+const cardImages = [
+  { src: "/images/beach1.jpg", matched: false },
+  { src: "/images/beach2.jpg", matched: false },
+  { src: "/images/beach3.jpg", matched: false },
+  { src: "/images/dandelion1.jpg", matched: false },
+  { src: "/images/dandelion2.jpg", matched: false },
+  { src: "/images/leaves1.jpg", matched: false },
+  { src: "/images/leaves2.jpg", matched: false },
+  { src: "/images/leaves3.jpg", matched: false },
+  { src: "/images/mountain1.jpg", matched: false },
+  { src: "/images/mountain2.jpg", matched: false },
+]
+
+function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
+
+  //shuffle cards
+  const shuffleCards = () => {
+    const shuffleCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+
+    setChoiceOne(null)
+    setChoiceTwo(null)
+      setCards(shuffleCards)
+    setTurns(0)
+  }
+
+  //handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  //compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true)
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        setTimeout(() => resetTurn(), 1000)
+      }
+    }
+  }, [choiceOne, choiceTwo])
+  
+  console.log(cards);
+
+  //rest choice & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns((prevTurns) => prevTurns + 1)
+    setDisabled(false)
+  }
+
+  //start new game automatically
+  useEffect(() => {
+    shuffleCards()
+  } , [])
+
+  return (
+    <div className="App">
+      <h1>Memorym game</h1>
+      <button onClick={shuffleCards}>New game</button>
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+      <p>Turns:{turns}</p>
+    </div>
+  )
+}
+
+export default App
